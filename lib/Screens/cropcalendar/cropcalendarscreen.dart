eae74158eb4636dbeffb87a3d7a5c2c07063ed09
madhavbhayani <madhavbhayani21@gmail.com>
2024-11-06 23:00:19 +0530

    Add Crop Calendar screen and import it in homescreen.dart

---
+++ lib/Screens/cropcalendar/cropcalendarscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CropCalenderScreen extends StatefulWidget {
+  const CropCalenderScreen({super.key});
+
+  @override
+  State<CropCalenderScreen> createState() => _CropCalenderScreenState();
+}
+
+class _CropCalenderScreenState extends State<CropCalenderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
